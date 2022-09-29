46da5009a872cb5ac7ed4805a4e9b1c11b98ee8a
IEATDIRT <114655348+IEATDIRTWASNTAVAILABLE@users.noreply.github.com>
2022-09-29 09:25:44 -0400

    Add files via upload

---
+++ src/simulation/elements/INCD.cpp
@@ -0,0 +1,43 @@
+#include "simulation/ElementCommon.h"
+
+void Element::Element_INCD()
+{
+	Identifier = "DEFAULT_PT_INCD";
+	Name = "INCD";
+	Colour = PIXPACK(0x303030);
+	MenuVisible = 1;
+	MenuSection = SC_SPECIAL;
+	Enabled = 1;
+
+	Advection = 0.0f;
+	AirDrag = 0.00f * CFDS;
+	AirLoss = 0.95f;
+	Loss = 0.00f;
+	Collision = 0.0f;
+	Gravity = 0.0f;
+	Diffusion = 0.00f;
+	HotAir = -0;
+	Falldown = 0;
+
+	Flammable = 0;
+	Explosive = 0;
+	Meltable = 0;
+	Hardness = 0;
+
+	Weight = 100;
+
+	DefaultProperties.temp = R_TEMP + 70.0f + 273.15f;
+	HeatConduct = 150;
+	Description = "Indestructible conductor, conducts SPRK..";
+
+	Properties = TYPE_SOLID | PROP_CONDUCTS | PROP_LIFE_DEC;
+
+	LowPressure = IPL;
+	LowPressureTransition = NT;
+	HighPressure = IPH;
+	HighPressureTransition = NT;
+	LowTemperature = ITL;
+	LowTemperatureTransition = NT;
+	HighTemperature = ITH;
+	HighTemperatureTransition = NT;
+}
